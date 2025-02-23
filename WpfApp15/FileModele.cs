6243ce723e602124f24a4a7d03082181934278e5
TimurYamchuk <134388477+TimurYamchuk@users.noreply.github.com>
2025-02-23 13:47:46 +0200

    Update FileModele.cs

--- WpfApp15/FileModele.cs
+++ WpfApp15/FileModele.cs
@@ -1,20 +1,24 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace WpfApp15
 {
-    public class FileModel
+    class FileModel
     {
-        public string Name { get; }
-        public string Folder { get; }
-        public long Size { get; }
-        public DateTime LastModified { get; }
+        public string Name { get; set; }
+        public string Folder { get; set; }
+        public string Size { get; set; }
+        public string Date { get; set; }
 
-        public FileModel(string name, string folder, long size, DateTime lastModified)
+        public FileModel(string name, string folder, string size, string data)
         {
             Name = name;
             Folder = folder;
             Size = size;
-            LastModified = lastModified;
+            Date = data;
         }
+
     }
-}
